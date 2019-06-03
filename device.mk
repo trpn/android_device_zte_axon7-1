dab8fb971655cdaab155373c9edbbe4786e635f2
DennySPB <dennyspb@gmail.com>
2019-06-03 04:31:18 +0200

    Add hostapd configs needed for CAF wlan

--- device.mk
+++ device.mk
@@ -228,6 +228,15 @@ PRODUCT_PACKAGES += \
     android.hidl.base@1.0 \
     android.hidl.manager@1.0
 
+# Hostapd
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/wifi/hostapd/hostapd.accept:system/etc/hostapd/hostapd.accept \
+    $(LOCAL_PATH)/wifi/hostapd/hostapd.deny:system/etc/hostapd/hostapd.deny \
+    $(LOCAL_PATH)/wifi/hostapd/hostapd_default.conf:system/etc/hostapd/hostapd_default.conf
+
+PRODUCT_PACKAGES += \
+    move_wifi_data.sh
+
 # IMS
 PRODUCT_PACKAGES += \
     ims-ext-common \
