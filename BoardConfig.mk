1ffc95e1506d7dd0f882f0f18b8c47b59e521a09
baddar <mnbaddar1990@gmail.com>
2019-08-19 13:18:10 +0400

    Skydragon bringup

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -90,6 +90,11 @@ TARGET_QCOM_DISPLAY_VARIANT := caf-msm8996
 TARGET_QCOM_MEDIA_VARIANT := caf-msm8996
 TARGET_QCOM_AUDIO_VARIANT := caf-msm8996
 
+PRODUCT_SOONG_NAMESPACES += \
+    hardware/qcom/display-$(TARGET_QCOM_DISPLAY_VARIANT) \
+    hardware/qcom/audio-$(TARGET_QCOM_AUDIO_VARIANT) \
+    hardware/qcom/media-$(TARGET_QCOM_MEDIA_VARIANT)
+
 # ANT+
 BOARD_ANT_WIRELESS_DEVICE := "qualcomm-hidl"
 
--- skydragon_axon7.mk
+++ skydragon_axon7.mk
@@ -15,35 +15,29 @@
 # limitations under the License.
 #
 
-# Inherit from those products. Most specific first.
-$(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
-
-# Get the prebuilt list of APNs
-$(call inherit-product, vendor/omni/config/gsm.mk)
 
 # Inherit from the common Open Source product configuration
+$(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/aosp_base_telephony.mk)
 
-#treble
-#$(call inherit-product, build/make/target/product/treble_common_64.mk)
-
-# must be before including omni part
-TARGET_BOOTANIMATION_SIZE := 1080p
+# Inherit some common SkyDragon stuff.
+$(call inherit-product, vendor/skydragon/config/common.mk)
 
 DEVICE_PACKAGE_OVERLAYS += device/zte/axon7/overlay
 DEVICE_PACKAGE_OVERLAYS += vendor/omni/overlay/CarrierConfig
 
-# Inherit some common Omni stuff.
-$(call inherit-product, vendor/omni/config/common.mk)
-
 # Inherit from ailsa_ii device
 $(call inherit-product, device/zte/axon7/device.mk)
 
+# Boot animation
+TARGET_SCREEN_HEIGHT := 2560
+TARGET_SCREEN_WIDTH := 1440
+
 # Device identifier. This must come after all inclusions.
-PRODUCT_NAME := omni_axon7
+PRODUCT_NAME := skydragon_axon7
 PRODUCT_DEVICE := axon7
 PRODUCT_BRAND := ZTE
-PRODUCT_MODEL := ZTE A2017U
+PRODUCT_MODEL := ZTE A2017X
 PRODUCT_MANUFACTURER := ZTE
 
 PRODUCT_BUILD_PROP_OVERRIDES += \
@@ -55,5 +49,3 @@ PRODUCT_BUILD_PROP_OVERRIDES += \
 BUILD_FINGERPRINT=OnePlus/OnePlus6/OnePlus6:9/PKQ1.180716.001/1905281230:user/release-keys
 
 PRODUCT_GMS_CLIENTID_BASE := android-zte
-
-ALLOW_MISSING_DEPENDENCIES := true
