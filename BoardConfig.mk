5c99ee3ec240626da0e46c7583c46a3ea13f01af
dianlujitao <dianlujitao@lineageos.org>
2019-09-08 13:39:42 +0800

    axon7: Remove custom kernel cross compile prefix
    Change-Id: Icc58f38fa2f66b833c73902d6733f84b77dcb575

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -77,7 +77,6 @@ TARGET_KERNEL_ARCH := arm64
 TARGET_KERNEL_HEADER_ARCH := arm64
 TARGET_KERNEL_SOURCE := kernel/zte/msm8996
 TARGET_KERNEL_CONFIG := lineageos_axon7_defconfig
-TARGET_KERNEL_CROSS_COMPILE_PREFIX := aarch64-linux-android-
 
 # Qualcomm support
 BOARD_USES_QCOM_HARDWARE := true
