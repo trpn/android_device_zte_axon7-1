7b1b3b379b74e26d8ae7129379a9e705e4d3f023
dianlujitao <dianlujitao@lineageos.org>
2019-09-14 18:48:25 +0800

    axon7: Hardcode NFC chip id to select the proper library
     * We don't ship nqnfcinfo and our NFC driver doesn't even support
       ioctls to read chip id, so hardcode it to use NQ NFC library instead
       of the default one for SN1XX.
    
    Change-Id: I43d9230aa1b8bd3efd5c0993a1c14f8381dd46cf

--- device.mk
+++ device.mk
@@ -375,7 +375,8 @@ PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/nfc/libnfc-nxp_RF.conf:$(TARGET_COPY_OUT_VENDOR)/etc/libnfc-nxp_RF.conf
 
 PRODUCT_PROPERTY_OVERRIDES += \
-    ro.hardware.nfc_nci=nqx.default
+    ro.hardware.nfc_nci=nqx.default \
+    vendor.qti.nfc.chipid=0x51
 
 # OMX
 PRODUCT_PACKAGES += \
