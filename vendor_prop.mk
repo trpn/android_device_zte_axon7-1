b50b5d1eb25136ed4256c858186bb46c8714759f
baddar <mnbaddar1990@gmail.com>
2019-10-11 21:59:41 +0400

    Axon7: Update Bluetooth blobs from LA.UM.7.5.r1-05300-8x96.0

--- vendor_prop.mk
+++ vendor_prop.mk
@@ -38,7 +38,7 @@ PRODUCT_PROPERTY_OVERRIDES += \
 # Bluetooth
 PRODUCT_PROPERTY_OVERRIDES += \
     bt.max.hfpclient.connections=1 \
-    qcom.bluetooth.soc=rome \
+    vendor.qcom.bluetooth.soc=rome \
     ro.bluetooth.emb_wp_mode=true \
     ro.bluetooth.wipower=true
 
