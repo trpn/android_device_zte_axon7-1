53a9b606eabd218430dc26cdb024ed222dd33375
baddar90 <mnbaddar1990@gmail.com>
2019-10-12 15:02:44 +0000

    add sensors conf

--- device.mk
+++ device.mk
@@ -398,6 +398,10 @@ PRODUCT_PACKAGES += \
     android.hardware.sensors@1.0-impl \
     android.hardware.sensors@1.0-service
 
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/configs/sensors/hals.conf:$(TARGET_COPY_OUT_VENDOR)/etc/sensors/hals.conf \
+    $(LOCAL_PATH)/configs/sensors/sensor_def_qcomdev.conf:$(TARGET_COPY_OUT_VENDOR)/etc/sensors/sensor_def_qcomdev.conf
+
 # TextClassifier
 PRODUCT_PACKAGES += \
     textclassifier.bundle1
