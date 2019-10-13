cc1bbc87012031db8bc2f342534569187c0ee201
baddar <mnbaddar1990@gmail.com>
2019-10-13 23:48:14 +0400

    remove soundtrigger

--- device.mk
+++ device.mk
@@ -113,7 +113,6 @@ PRODUCT_PACKAGES += \
     android.hardware.audio.effect@5.0 \
     android.hardware.audio.effect@5.0-impl \
     android.hardware.audio.effect@2.0-service \
-    android.hardware.soundtrigger@2.1-service \
     android.hardware.soundtrigger@2.1-impl \
     audio.a2dp.default \
     audio_amplifier.msm8996 \
