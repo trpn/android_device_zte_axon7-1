7a7fc5821558e4a3936e941a1c632f55bceaf2e6
dianlujitao <dianlujitao@lineageos.org>
2019-10-18 11:05:40 +0800

    msm8996-common: Enable QC framework value-adds
    Change-Id: I1987d3509b3db7cd4204537a6f2dc2ff4c611278
    Signed-off-by: xNombre <kartapolska@gmail.com>

--- device.mk
+++ device.mk
@@ -359,6 +359,12 @@ PRODUCT_DEFAULT_PROPERTY_OVERRIDES += \
 PRODUCT_ODM_PROPERTIES += \
     ro.vendor.qti.va_odm.support=1
 
+PRODUCT_DEFAULT_PROPERTY_OVERRIDES += \
+    ro.vendor.qti.va_aosp.support=1
+
+PRODUCT_ODM_PROPERTIES += \
+    ro.vendor.qti.va_odm.support=1
+
 # QMI
 PRODUCT_PACKAGES += \
     libjson
