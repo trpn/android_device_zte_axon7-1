6181e3f90a85ad40cf3bf956cca2392193127305
baddar <mnbaddar1990@gmail.com>
2019-10-19 14:46:10 +0400

    Proper fix thermals in pie *Revert to daisy blobs. *Build thermal hal *remove "-8996" from .conf name & give proper permissions. *Add sepoilcy rules just incase.

--- device.mk
+++ device.mk
@@ -388,8 +388,13 @@ PRODUCT_PACKAGES += \
     textclassifier.bundle1
 
 # Thermal
+PRODUCT_PACKAGES += \
+    android.hardware.thermal@1.0-impl \
+    android.hardware.thermal@1.0-service \
+    thermal.msm8996
+
 PRODUCT_COPY_FILES += \
-    $(LOCAL_PATH)/configs/thermal-engine-8996.conf:$(TARGET_COPY_OUT_VENDOR)/etc/thermal-engine-8996.conf
+    $(LOCAL_PATH)/configs/thermal-engine.conf:$(TARGET_COPY_OUT_VENDOR)/etc/thermal-engine.conf
 
 # Trust HAL
 PRODUCT_PACKAGES += \
