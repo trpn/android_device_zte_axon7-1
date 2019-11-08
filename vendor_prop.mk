270b98f5d2deb2be1a4f62dc21949ffa4784be6f
Bladeyy <getsuga369@gmail.com>
2019-11-08 07:13:19 +0400

    Axon7: Enable USB debugging at boot

--- vendor_prop.mk
+++ vendor_prop.mk
@@ -176,6 +176,13 @@ PRODUCT_PROPERTY_OVERRIDES += \
 PRODUCT_PROPERTY_OVERRIDES += \
     persist.timed.enable=true
 
+# USB debugging
+PRODUCT_PROPERTY_OVERRIDES += \
+    persist.sys.usb.config=mtp,adb \
+    ro.adb.secure=0 \
+    ro.secure=0 \
+    ro.debuggable=1
+
 # VNDK
 PRODUCT_DEFAULT_PROPERTY_OVERRIDES += \
     ro.vndk.version=27
