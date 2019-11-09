646e5026510718f0fee3a8a38f03b47a100bc697
baddar90 <mnbaddar1990@gmail.com>
2019-11-09 05:40:37 +0000

    skip selinux and neverallow

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -68,6 +68,7 @@ DEVICE_MANIFEST_FILE := $(PLATFORM_PATH)/manifest.xml
 # Kernel
 BOARD_KERNEL_CMDLINE := androidboot.hardware=qcom user_debug=31 msm_rtb.filter=0x237 ehci-hcd.park=3 lpm_levels.sleep_disabled=1 cma=32M@0-0xffffffff
 BOARD_KERNEL_CMDLINE += loop.max_part=7
+BOARD_KERNEL_CMDLINE += androidboot.selinux=permissive
 BOARD_KERNEL_BASE := 0x80000000
 BOARD_KERNEL_PAGESIZE := 4096
 BOARD_KERNEL_TAGS_OFFSET := 0x00000100
@@ -218,9 +219,12 @@ TARGET_RELEASETOOLS_EXTENSIONS := $(PLATFORM_PATH)
 VENDOR_SECURITY_PATCH := 2018-07-01
 
 # SELinux
-include device/qcom/sepolicy-legacy-um/sepolicy.mk
+#include device/qcom/sepolicy-legacy-um/sepolicy.mk
 
-BOARD_SEPOLICY_DIRS += $(PLATFORM_PATH)/sepolicy
+#BOARD_SEPOLICY_DIRS += $(PLATFORM_PATH)/sepolicy
+
+# NEVER ALLOW
+SELINUX_IGNORE_NEVERALLOWS := true
 
 # Wifi
 BOARD_USES_AOSP_WLAN_HAL := true
