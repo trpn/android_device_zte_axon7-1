25a68f3a8c7e60d3b7b1af53e3274f48a59c7f3c
baddar90 <mnbaddar1990@gmail.com>
2019-11-22 09:22:12 +0000

    arrow bringup

--- arrow_axon7.mk
+++ arrow_axon7.mk
@@ -22,7 +22,7 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 $(call inherit-product, device/zte/axon7/device.mk)
 
 # Inherit some common PixelExperience stuff.
-$(call inherit-product, vendor/aosp/config/common_full_phone.mk)
+$(call inherit-product, vendor/arrow/config/common.mk)
 
 # Set gapps arch
 TARGET_GAPPS_ARCH := arm64
@@ -31,7 +31,7 @@ TARGET_GAPPS_ARCH := arm64
 TARGET_BOOT_ANIMATION_RES := 1440
 
 # Device identifier. This must come after all inclusions.
-PRODUCT_NAME := aosp_axon7
+PRODUCT_NAME := arrow_axon7
 PRODUCT_DEVICE := axon7
 PRODUCT_BRAND := ZTE
 PRODUCT_MODEL := ZTE A2017X
