e136468a3cfdefb3e6beb99fd656e31550371f0a
baddar90 <mnbaddar1990@gmail.com>
2019-12-10 18:33:24 +0400

    Revert "axon7: Revert of treblizing this platform"
    This reverts commit 3d558bc82c069217fc6bfe42d2ad730826ae16a8.

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -224,6 +224,11 @@ include device/qcom/sepolicy/sepolicy.mk
 
 BOARD_SEPOLICY_DIRS += $(PLATFORM_PATH)/sepolicy
 
+# Treble
+BOARD_VNDK_RUNTIME_DISABLE := true
+BOARD_VNDK_VERSION := current
+PRODUCT_FULL_TREBLE_OVERRIDE := true
+
 # Wifi
 BOARD_HAS_QCOM_WLAN := true
 BOARD_HAS_QCOM_WLAN_SDK := true
