419cfc49b017e991fa4522176c2d5854b2c035ab
xNombre <kartapolska@gmail.com>
2019-12-15 19:21:15 +0400

    Axon7: rootdir: port for android device
    This adds device-specific changes, configfs script and separate device script.

--- rootdir/Android.mk
+++ rootdir/Android.mk
@@ -100,7 +100,7 @@ LOCAL_MODULE_PATH  := $(TARGET_OUT_VENDOR_EXECUTABLES)
 include $(BUILD_PREBUILT)
 
 include $(CLEAR_VARS)
-LOCAL_MODULE        := init.target.rc
+LOCAL_MODULE        := init.device.rc
 LOCAL_MODULE_TAGS   := optional eng
 LOCAL_MODULE_CLASS  := ETC
 LOCAL_SRC_FILES     := etc/init.target.rc
